78a85a01bce2f7ec151e4ddf51c0fd7c77319420
Ashwani <ashwani.shakya@lyxellabs.com>
2018-04-18 13:16:36 +0530

    Runserver re-build

--- falcon_project/runserver.py
+++ falcon_project/runserver.py
@@ -1,4 +1,5 @@
 import falcon
+from werkzeug.serving import run_simple
 
 from app import *
 
@@ -8,3 +9,6 @@ api.add_route('/api/v1/Register/', RegisterClass())
 api.add_route('/api/v1/Upload/', UploadClass())
 api.add_route('/api/v1/Detail/', DetailClass())
 api.add_route('/api/v1/verify/', OptVerificationClass())
+
+if __name__ == '__main__':
+    run_simple('0.0.0.0', 5000, api, use_reloader=True)
